da57157b2b0bf152e5004507dffeea6b92d130c6
Tahsin <tahsin.anam@tietoevry.com>
2022-11-17 11:49:28 +0200

    edited pom.xml

---
+++ InsertData/src/main/java/InsertToSql.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class InsertToSql {
+    public static void main(String[] args) {
+
+    }
+
+    public static Connection getConnection(){
+
+        try {
+            String driver = "";
+        }
+        catch (){
+
+        }
+
+
+        return null;
+    }
+
+}
